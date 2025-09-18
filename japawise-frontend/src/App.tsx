5bf59307b1278a38b0b224433691ef3460800a16
James Ofor <oforjames2k22@outlook.com>
2025-09-18 15:33:33 +0100

    just testing things

--- japawise-frontend/src/App.tsx
+++ japawise-frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 
-// Simple router implementation
-// In a real app, you'd likely use React Router or similar
 const App: React.FC = () => {
-  // Basic routing logic
-  const path = window.location.pathname;
-  
   return (
     <Layout>
-      {path === '/about' ? <AboutPage /> : <HomePage />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        {/* Add a catch-all route for 404s */}
+        <Route path="*" element={<HomePage />} />
+      </Routes>
     </Layout>
   );
 };
